e0443fe8ca254b89192ce2156fa7c189306b45d5
Ralica Popova <ralicaterzieva90@gmail.com>
2024-12-01 18:26:51 +0200

    Add more puppies classes logic

--- app.py
+++ app.py
@@ -5,10 +5,40 @@ app = Flask(__name__)
 
 api = Api(app)
 
+puppies = []
+
+class PuppyName(Resource):
+    def get(self, name):
+        for pup in puppies:
+            if pup['name'] == name:
+                return pup
+            
+        return {'name': None}
+
+    def post(self, name):
+        pup = {'name': name}
+
+        puppies.append(pup)
+        return pup
+
+    def delete(self, name):
+        for ind,pup in enumerate(puppies):
+            if pup['name'] == name:
+                deleted_pup = puppies.pop(ind)
+                return {'note': 'Delete Success!'}
+            
+class AllNames(Resource):
+    
+    def get(self):
+        return {'puppies': puppies}
+
 class HelloWorld(Resource):
 
     def get(self):
         return {'hello': 'world'}
+    
+
+api.add_resource(HelloWorld, '/')
 
 
 if __name__ == '__main__':
